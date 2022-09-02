d2f40ffc10ecd062c72e73ad57e51b1a18aae08d
Melroy <melroydsilva4@gmail.com>
2022-09-02 16:20:38 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import { Badge } from '@material-ui/core'
+import { Search,ShoppingCartOutlined } from '@material-ui/icons'
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import React from 'react'
+import styled from 'styled-components'
+import { mobile } from '../responsive'
+
+const Container=styled.div`
+height: 60px;
+${mobile({height: "50px"})}
+
+`
+
+const Wrapper=styled.div`
+padding: 10px 20px;
+display:flex;
+
+align-items:center;
+justify-content: space-between;
+${mobile({padding: "10px 0px"})}
+
+`
+const Left=styled.div`
+flex:1;
+display:flex;
+align-items:center;
+
+`
+const Language=styled.span`
+font-size:14px;
+cursor: pointer;
+${mobile({display: "none"})}
+`
+const SearchContainer=styled.div`
+border: 0.5px solid lightgray;
+display: flex;
+align-items: center;
+margin-left: 50px;
+padding: 5px;
+padding-left:140px
+
+
+`
+const Input=styled.div`
+border:none;
+${mobile({width: "50px"})}
+`
+const Center=styled.div`
+text-align: center;
+
+flex:1;`
+const Right=styled.div`
+flex:1;
+display:flex;
+align-items:center;
+justify-content:flex-end;
+${mobile({justifyContent: "center",flex:"2"})}
+`
+const Logo=styled.h1`
+font-weight:bold;
+${mobile({fontSize: "24px"})}
+`
+const Menuitem=styled.div`
+font-size:14px;
+cursor:pointer;
+margin-left:25px;
+${mobile({fontSize: "12px",marginLeft:"10px"})}
+
+`
+const Navbar = () => {
+  return (
+   
+    <Container>
+<Wrapper>
+    <Left>
+        <Language>EN</Language>
+        <SearchContainer>
+        <Input placeholder="Search"/>
+        
+            <Search style={{color:"gray",fontSize:16}}/>
+                
+
+            
+    
+        </SearchContainer>
+    </Left>
+    <Center><Logo>Ecommerce Website</Logo></Center>
+    <Right>
+<Menuitem>REGISTER</Menuitem>
+<Menuitem>SIGN IN</Menuitem>
+<Menuitem>
+<Badge badgeContent={4} color="primary">
+        <ShoppingCartOutlined />
+      </Badge>
+</Menuitem>
+
+    </Right>
+</Wrapper>
+
+    </Container>
+   
+  )
+}
+
+export default Navbar
